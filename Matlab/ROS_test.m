a655507597ce328c6688a22f951eac2703732afc
Haoran-Zhao <zhaohaorandl@gmail.com>
2020-09-15 16:46:44 -0500

    Add files via upload

---
+++ Matlab/ROS_test.m
@@ -0,0 +1,16 @@
+setenv('ROS_MASTER_URI','http://192.168.1.2:11311')
+setenv('ROS_IP','192.168.1.3')
+
+rosshutdown;
+IP = '192.168.1.2';
+rosinit(IP,11311);
+
+ftSub = rossubscriber('/ft_data');
+
+while(1)
+    msg_ft = ftSub.LatestMessage;
+    ft_vec(1) = round(msg_ft.Linear.X, 1);
+    ft_vec(2) = round(msg_ft.Linear.Y, 1);
+    ft_vec(3) = round(msg_ft.Linear.Z, 1);
+    disp(ft_vec(3));
+end
