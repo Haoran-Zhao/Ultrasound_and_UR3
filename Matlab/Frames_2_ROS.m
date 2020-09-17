ddff3c275a049489a0496a04a38456e5f454583e
Haoran-Zhao <zhaohaorandl@gmail.com>
2020-09-17 16:32:09 -0700

    Add files via upload

--- Matlab/Frames_2_ROS.m
+++ Matlab/Frames_2_ROS.m
@@ -12,13 +12,47 @@ rosinit(IP,11311);
 iter=1;
 
 while(1)
-    filename = [pwd '/Verasonics_Haoran/Images/scene',num2str(iter),'.png'];
-    if isfile(filename)
-        image = imread(filename);
-        imshow(image);
-        frameMsg.Encoding = 'mono8';
-        writeImage(frameMsg,image);
-        send(framePub, frameMsg);
-        iter = iter + 1;
+%     filename = [pwd '/Verasonics_Haoran/Images/scene',num2str(iter),'.png'];
+    directory = [pwd '/Verasonics_Haoran/Images'];
+    
+    dirc = dir(directory);
+    %Filter out all the folders.
+    dirc = dirc(~cellfun(@isfolder,{dirc(:).name}));
+    if ~isempty(dirc)
+        imagename = getlatestfile(dirc);
+        filename = [directory,'/',imagename];
+%         filename = ['C:\Users\Julien Leclerc\Documents\Vantage-4.1.1-1910101200/Verasonics_Haoran/Images/scene2913.png'];
+        if isfile(filename)
+            image = imread(filename);
+%             imshow(image);
+            frameMsg.Encoding = 'mono8';
+            writeImage(frameMsg,image);
+            send(framePub, frameMsg);
+%             iter = iter + 1;
+        end
     end
+end
+
+
+function latestfile = getlatestfile(directory)
+%This function returns the latest file from the directory passsed as input
+%argument
+
+% %Get the directory contents
+% dirc = dir(directory);
+% 
+% %Filter out all the folders.
+% dirc = dirc(~cellfun(@isfolder,{dirc(:).name}));
+
+%I contains the index to the biggest number which is the latest file
+% [~,I] = max([directory(:).datenum]);
+
+    names = regexp([directory(:).name], '(?<=scene)\d+', 'match');
+    namesnum = cellfun(@str2num, names);
+    [~,I] = max(namesnum);
+
+    if ~isempty(I)
+        latestfile = directory(I).name;
+    end
+
 end
